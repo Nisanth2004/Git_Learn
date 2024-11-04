713edfb2b56bfb206a8c9c3ea3624a4ca5f6b6f9
Nisanth2004 <nisanthjothi2004@gmail.com>
2024-11-04 18:31:34 +0530

    add a project

---
+++ src/main/java/com/example/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class HelloWorld {
+    public static void main(String[] args)
+    {
+        System.out.printf("Hello world");
+        System.out.println("Sujith");
+    }
+}
